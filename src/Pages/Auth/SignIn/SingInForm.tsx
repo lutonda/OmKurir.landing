b5d24f2a717eb4082827878e6fcf63056d3b5ac5
Sebastião Dias Lutonda <lutonda@gmail.com>
2022-05-29 22:41:08 +0000

    signin and singon

--- src/Pages/Auth/SignIn/SingInForm.tsx
+++ src/Pages/Auth/SignIn/SingInForm.tsx
@@ -1,31 +1,37 @@
 import React, { Component, useState } from "react";
 import { Form, Row, Button, Col, Container } from "react-bootstrap";
+import GoogleLogin from "react-google-login";
 import { FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
 
 function SingInForm() {
   return (
-    
-      <div className="form-wrapper-content">
-        <div className="content">
-          <div className="col-md-12">
-            <div className="title" style={{ textAlign: "left" }}>
-              <h2>
-                <FormattedMessage id={"app.contactUs.form"} />
-              </h2>
-              <FormattedMessage id={"app.contactUs.formdescription"} />
-            </div>
+    <div className="form-wrapper-content">
+      <div className="content">
+        <div className="col-md-12">
+          <div className="title" style={{ textAlign: "left" }}>
+            <h2>
+              <FormattedMessage id={"app.contactUs.form"} />
+            </h2>
+            <FormattedMessage id={"app.contactUs.formdescription"} />
+          </div>
 
-            <div className="subtitle"></div>
-          </div>
-          <div style={{ textAlign: "left" }}>
-            <SimpleForm />
-          </div>
+          <div className="subtitle"></div>
+        </div>
+        <div style={{ textAlign: "left" }}>
+          <SimpleForm />
         </div>
       </div>
+    </div>
   );
 }
 
+const handleFailure = (result: any) => {
+  alert(JSON.stringify(result));
+};
+const handleLogin = (result: any) => {
+  alert(JSON.stringify(result));
+};
 function SimpleForm() {
   const initialState = {
     email: "",
@@ -37,7 +43,7 @@ function SimpleForm() {
     const obj: any = { [event.target.name]: event.target.value };
     setState(obj);
   };
-
+  const { GOOGLE_API_CLIENT_KEY } = process.env;
   const { email, password } = state;
 
   return (
@@ -72,9 +78,17 @@ function SimpleForm() {
       <Button variant="primary" className="main-btn wow fadeInUp">
         Sing In
       </Button>
-      <br/><br/>
+      <GoogleLogin
+        clientId={GOOGLE_API_CLIENT_KEY??''}
+        buttonText="Login with Google"
+        onSuccess={handleLogin}
+        onFailure={handleFailure}
+        cookiePolicy="single_host_origin"
+      ></GoogleLogin>
+      <br />
+      <br />
       <Link className="wow fadeInUp" to="/auth/join">
-        Create an Account
+        Create an Account {process.env.GOOGLE_API_CLIENT_KEY}
       </Link>
     </Container>
   );
